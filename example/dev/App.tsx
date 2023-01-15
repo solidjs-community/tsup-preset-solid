30e019a4e6fed894c9c2f6ffccc9636892835dfa
thetarnav <thetarnav@users.noreply.github.com>
2023-01-15 21:27:49 +0000

    Format

--- example/dev/App.tsx
+++ example/dev/App.tsx
@@ -1,7 +1,7 @@
 import type { Component } from 'solid-js'
-import logo from './logo.svg'
-import styles from './App.module.css'
 import { Hello } from '../src'
+import styles from './App.module.css'
+import logo from './logo.svg'
 
 const App: Component = () => {
   return (
